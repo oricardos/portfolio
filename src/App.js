0a1a18b9c40e0f6377861da0f4249796e7e016f2
oricardos <oricardo.dev@gmail.com>
2024-05-16 23:56:31 -0300

    update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import { Layout } from './components/Layout';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { Projects } from './components/pages/Projects';
 import { NotFound } from './components/pages/NotFound';
 import style from './App.module.css';
 import { Home } from './components/Home';
-import { Blog } from './components/pages/Blog';
 import { BlogView } from './components/pages/BlogView';
 
 function App() {
@@ -12,7 +10,7 @@ function App() {
         <div className={style.__wrapper}>
             <Router>
                 <Routes>
-                    <Route path="/" element={<Home />} />
+                    <Route path="/portfolio" element={<Home />} />
                     <Route path="/" element={<Layout />}>
                         <Route path="blog">
                             <Route path=":slug" element={<BlogView />} />
